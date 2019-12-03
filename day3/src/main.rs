567a561b50ff19335b25fc07574a6d5faf9caa70
Adrian Chitescu <adrian.chitesc90@gmail.com>
2019-12-03 21:09:18 +0200

    implemented part1 & part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::fs;
-
+use std::fmt;
 
 enum Direction {
     Right,
@@ -8,19 +8,123 @@ enum Direction {
     Up,
     Down
 }
-struct Point(i32, i32);
+#[derive(Copy,Clone)]
+struct Point {
+    x: i32,
+    y: i32
+}
+
+struct Segment {
+    a: Point,
+    b: Point,
+    horizontal : bool,
+    original_order: bool
+}
+impl Segment {
+    fn new(p1: Point, p2: Point) -> Segment {
+        if p1.y == p2.y {
+            if p1.x < p2.x {
+                Segment { a: p1, b: p2, horizontal: true, original_order: true}
+            } else {
+                Segment { a: p2, b: p1, horizontal: true, original_order: false}
+            }
+        } else {
+            if p1.y < p2.y {
+                Segment { a: p1, b: p2, horizontal: false, original_order: true}
+            }else {
+                Segment { a: p2, b: p1, horizontal: false, original_order: false}
+            }
+        }
+    }
+
+    fn on_x(&self, p: &Point) -> bool {
+        self.horizontal && self.a.x <= p.x && p.x <= self.b.x
+    }
+    fn on_y(&self, p: &Point) -> bool {
+        (!self.horizontal) && self.a.y <= p.y && p.y <= self.b.y
+    }
+
+    fn intersect(&self, s: &Segment) -> Option<Point> {
+        if self.horizontal == s.horizontal {
+            None
+        } else {
+            if self.horizontal {
+                if self.on_x(&s.a) && s.on_y(&self.a) {
+                    Some(Point {x : s.a.x, y: self.a.y})
+                } else {
+                    None
+                }
+            } else {
+                if self.on_y(&s.a) && s.on_x(&self.b) {
+                    Some(Point { x: self.a.x, y: s.a.y })
+                } else {
+                    None
+                }
+            }
+        }
+    }
+    fn length(&self) -> i32 {
+        (self.a.x - self.b.x).abs() + (self.a.y - self.b.y).abs()
+    }
+
+    fn dist_to(&self, p: &Point) -> Option<i32> {
+        if self.on_x(p) {
+            if self.a.y == p.y {
+                if self.original_order {
+                    Some(p.x - self.a.x)
+                } else {
+                    Some(self.b.x - p.x)
+                }
+            } else {
+                None
+            }
+        } else {
+            if self.on_y(p) {
+                if self.a.x == p.x {
+                    if self.original_order {
+                        Some(p.y - self.a.y)
+                    } else {
+                        Some(self.b.y - p.y)
+                    }
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        }
+    }
+}
+
 struct Wire {
     direction : Direction,
     length : i32
 }
 
-fn parse_input(input: &str) {
+fn man_dist(p: &Point) -> i32 {
+    p.x.abs() + p.y.abs()
+}
+
+fn dist_to_intersection(vec: &Vec<Segment>, p : &Point) -> i32 {
+    let mut total_dist = 0;
+    for s in vec.iter() {
+        if let Some(d) = s.dist_to(p) {
+            total_dist += d;
+            break;
+        } else {
+            total_dist += s.length();
+        }
+    }
+    total_dist
+}
+
+fn parse_input(input: &str) -> Vec<Vec<Wire>> {
     input
         .lines()
         .map(|line| {
             line.split(",")
                 .map(|s| Wire {
-                    direction : match s.char().next().unwrap() {
+                    direction : match s.chars().next().unwrap() {
                         'U' => Direction::Up,
                         'R' => Direction::Right,
                         'D' => Direction::Down,
@@ -34,30 +138,55 @@ fn parse_input(input: &str) {
                 })
                 .collect()
         })
+        .collect()
 }
 
-
-
-fn part2(vec: &Vec<i32>) -> () {
-    let mut noun = 0;
-    let mut verb = 0;
-
-    let target = 19690720;
-    while part1(vec, noun, verb) < target {
-        noun += 1;
+fn generate_segments(wire : &Vec<Wire>) -> Vec<Segment> {
+    let mut p: Point = Point { x: 0, y: 0 };
+    let mut points = vec![];
+    points.push(p);
+    for w in wire {
+        match w.direction {
+            Direction::Left => points.push(Point { x: p.x - w.length, y: p.y}),
+            Direction::Right => points.push(Point { x: p.x + w.length, y: p.y}),
+            Direction::Up => points.push( Point { x: p.x, y: p.y + w.length}),
+            Direction::Down => points.push(Point { x: p.x, y: p.y - w.length})
+        }
+        p.x = points.last().unwrap().x;
+        p.y = points.last().unwrap().y;
+    }
+    let mut segments = vec![];
+    for (p1, p2) in points.iter().zip(points[1..].iter()) {
+        segments.push(Segment::new((p1.clone()), p2.clone()));
     }
 
-    noun -= 1;
-    while part1(vec, noun, verb) < target {
-        verb += 1;
-    }
-
-    println!("Values : {} and {}", noun, verb);
+    segments
 }
 
+fn part1(wire1: &Vec<Wire>, wire2: &Vec<Wire>) -> (i32, i32) {
+    let seg1 = generate_segments(wire1);
+    let seg2 = generate_segments(wire2);
 
-fn part1(v: &Vec<Wire>) -> i32 {
+    let mut intersections = vec![];
+    let mut dist_inter = vec![];
+    for s1 in &seg1 {
+        for s2 in &seg2 {
+            if let Some(p) = s1.intersect(&s2) {
+                if p.x != 0 && p.y != 0 {
+                    intersections.push(man_dist(&p));
+                    dist_inter.push(
+                        dist_to_intersection(&seg1, &p) + dist_to_intersection(&seg2, &p));
+                }
+            }
+        }
+    }
 
+    let p1 = *intersections.iter()
+        .min_by(|x,y| x.cmp(y)).unwrap_or(&0);
+    let p2 = *dist_inter.iter()
+        .min_by(|x,y| x.cmp(y)).unwrap_or(&0);
+
+    (p1, p2)
 }
 
 fn main() {
@@ -73,7 +202,5 @@ fn main() {
 
     let vec = parse_input(&file_contents);
 
-//    println!("The total fuel needed is {}", part1(&vec,72, 94));
-//    println!("The total fuelify  is {}", part2(&vec));
-    part2(&vec);
+    println!("The min distance is {:?}", part1(&vec[0], &vec[1]));
 }
