27f9df66c2ea3fa1f0b28cd77d935bbcca9b44f3
Adrian Chitescu <achitescu@bitdefender.com>
2019-12-05 19:00:10 +0200

    cargo-fmt

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -16,7 +16,7 @@ enum InstructionType {
 #[derive(Debug)]
 struct Instruction {
     itype: InstructionType,
-    operands : Vec<usize>,
+    operands: Vec<usize>,
 }
 
 fn parse_input(input: &str) -> Vec<i32> {
@@ -31,39 +31,68 @@ fn parse_input(input: &str) -> Vec<i32> {
     }
     vec
 }
-fn get_positions(p:&Vec<i32>, ip: &mut usize, n: usize) -> Vec<usize> {
+fn get_positions(p: &Vec<i32>, ip: &mut usize, n: usize) -> Vec<usize> {
     let mut o = vec![0; n];
     let mut op = p[*ip as usize] / 100;
     let mut i = 0;
     *ip += 1;
     while i < n {
-       if op % 10 == 0 {
-          o[i] = p[*ip as usize] as usize;
-       } else {
-          o[i] = *ip as usize;
-       }
-       *ip += 1;
+        if op % 10 == 0 {
+            o[i] = p[*ip as usize] as usize;
+        } else {
+            o[i] = *ip as usize;
+        }
+        *ip += 1;
         i += 1;
         op /= 10;
     }
 
     o
 }
-fn get_instruction(p: & Vec<i32>, ip: &mut usize) -> Instruction {
+fn get_instruction(p: &Vec<i32>, ip: &mut usize) -> Instruction {
     use InstructionType::*;
     let operation = p[*ip];
-    let mut instr: Instruction = Instruction { itype: InstructionType::Add, operands: vec![]};
+    let mut instr: Instruction = Instruction {
+        itype: InstructionType::Add,
+        operands: vec![],
+    };
     match operation % 100 {
-        1 => { instr.itype = Add;           instr.operands = get_positions(p, ip, 3);}
-        2 => { instr.itype = Multiply;      instr.operands = get_positions(p, ip, 3);}
-        3 => { instr.itype = Input;         instr.operands = get_positions(p, ip, 1);}
-        4 => { instr.itype = Output;        instr.operands = get_positions(p, ip, 1);}
-        5 => { instr.itype = JumpIfTrue;    instr.operands = get_positions(p, ip, 2);}
-        6 => { instr.itype = JumpIfFalse;   instr.operands = get_positions(p, ip, 2);}
-        7 => { instr.itype = LessThan;      instr.operands = get_positions(p, ip, 3);}
-        8 => { instr.itype = Equals;        instr.operands = get_positions(p, ip, 3);}
-        99 =>{ instr.itype = Exit;}
-        _ => unreachable!()
+        1 => {
+            instr.itype = Add;
+            instr.operands = get_positions(p, ip, 3);
+        }
+        2 => {
+            instr.itype = Multiply;
+            instr.operands = get_positions(p, ip, 3);
+        }
+        3 => {
+            instr.itype = Input;
+            instr.operands = get_positions(p, ip, 1);
+        }
+        4 => {
+            instr.itype = Output;
+            instr.operands = get_positions(p, ip, 1);
+        }
+        5 => {
+            instr.itype = JumpIfTrue;
+            instr.operands = get_positions(p, ip, 2);
+        }
+        6 => {
+            instr.itype = JumpIfFalse;
+            instr.operands = get_positions(p, ip, 2);
+        }
+        7 => {
+            instr.itype = LessThan;
+            instr.operands = get_positions(p, ip, 3);
+        }
+        8 => {
+            instr.itype = Equals;
+            instr.operands = get_positions(p, ip, 3);
+        }
+        99 => {
+            instr.itype = Exit;
+        }
+        _ => unreachable!(),
     };
 
     instr
@@ -99,7 +128,7 @@ fn run(v: &Vec<i32>, input: i32) -> i32 {
                 dest = instr.operands[0];
                 vec[dest] = input;
             }
-            JumpIfTrue  => {
+            JumpIfTrue => {
                 if vec[instr.operands[0]] != 0 {
                     pos = vec[instr.operands[1]] as usize;
                 }
@@ -124,7 +153,7 @@ fn run(v: &Vec<i32>, input: i32) -> i32 {
                 }
             }
 
-            _ => unreachable!()
+            _ => unreachable!(),
         };
     }
 
@@ -134,12 +163,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let input_filename = args[1].clone();
 
-    let file_contents = fs::read_to_string(&input_filename)
-        .unwrap_or_else(|err| {
-            eprintln!("Error : {}", err);
-            eprintln!("Cannot read from file {}", input_filename);
-            std::process::exit(1);
-        });
+    let file_contents = fs::read_to_string(&input_filename).unwrap_or_else(|err| {
+        eprintln!("Error : {}", err);
+        eprintln!("Cannot read from file {}", input_filename);
+        std::process::exit(1);
+    });
 
     let vec1 = parse_input(&file_contents);
     let vec2 = vec1.clone();
